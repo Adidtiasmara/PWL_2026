2b15dc341078c4885bceeefbdac76e58fb9a4a45
Muhammad Firman Aditiasmara <mfirmanadid@gmail.com>
2026-02-25 11:26:40 +0700

    upd: praktikum progress

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,13 +6,5 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello (){
-        return 'Anjay Mabar';
-    }
-
-    public function greeting(){
-        // return view('blog.hello', ['name' => 'Adid']);
-        return view('blog.hello')->with('name', 'Adid')
-        ->with('occupation', 'College Student');
-    }
+    //
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/opening', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function ($name='Muhammad Firman Aditiasmara', $nim='244107020094') {
+    return 'Nama saya '.$name .' dengan NIM '.$nim;
+});
+
+
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PhotoController;
+
+Route::get('/user/{name?}', function ($name) {
+return 'Nama saya '.$name;
+});
+
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id, ) {
+    return 'Halaman Artikel dengan ID: '.$id;
+});
+
+// Route::get('/user/{name?}', function ($name=null) {
+// return 'Nama saya '.$name;
+// });
 
 Route::get('/user/{name?}', function ($name='John') {
 return 'Nama saya '.$name;
 });
 
-Route::get('/hello', [WelcomeController::class,'hello']);
 
-Route::get('/about', [PageController::class,'about']);
-Route::get('/articles/{id}', [PageController::class,'articles']);
+
+// Route::get('/about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class,'articles']);
 
 
 Route::resource('photos', PhotoController::class)->except([
